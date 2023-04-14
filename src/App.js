9fc83e7c88e928b98e05e2923edb3c3a7ba33799
Monusagar <71325166+Monusagar@users.noreply.github.com>
2023-04-14 18:33:28 +0000

    add nev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+function Home(){
+ const [n,setn]= useState(0)
+  return(<div>
+    <button onClick={()=> setn(n-1)}>-</button><span>{n}</span><button onClick={()=> setn(n+1)}>+</button>
+  </div>)
+}
+
+function SignIn(){
+  return(<div>SignIn</div>)
+}
+
+function SignUp(){
+  return(<div>SignUp</div>)
+}
+
+function Profile(){
+  return(<div>Profile</div>)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/SignIn" element={<SignIn />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/UserProfile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
